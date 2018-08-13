a7f1c872123a3791ec33c013bd32cdfe373531a4
tenderribs <dc3dude@gmail.com>
2018-08-13 18:46:37 +0200

    my name jeff

--- pages/home.php
+++ pages/home.php
@@ -1,13 +1,11 @@
 <div class="tile is-ancestor">
-    <div class="tile is-parent is-4">
+    <div class="tile is-parent is-5">
         <article class="tile is-child box" class="message is-dark">
             <div class="message-body">
-
-                        <figure class="image" style="width: 50px;" align="left">
-                            <img src="images/favicon/android-icon-144x144.png">
-                        </figure>
-
-                        <h1 class="title" align="right">Pfadi Hochwacht Baden</h1>
+                    <p style="float: left;" align="center"><img src="images/favicon/android-icon-48x48.png"></p>
+                    <p class="title" align="center">Pfadi Hochwacht Baden</p>
+                    <hr>
+                    <p>Willkommen</p>
             </div>
         </article>
     </div>
@@ -16,15 +14,16 @@
             <div class="message-body">
 
                 <p class="title">Lesenswerte Artikel:</p>
+                <hr>
                 <a href="//nebula.wsimg.com/3401e06a3a799d3ea907a4be745e31c5?AccessKeyId=7DA39F302406C2225647" target="_blank">
                     <span class="button is-dark">
-                        "Mit der Pfadi gesünder durchs Leben" AZ 2. Dezember 2016
+                        "Mit der Pfadi gesünder durchs Leben" AZ
                     </span>
                 </a>
                 </a>
                 <a href="//nebula.wsimg.com/459fbb619f26139b785243fcbbab3d4f?AccessKeyId=7DA39F302406C2225647" target="_blank">
                     <span class="button is-dark">
-                        "Wartelisten bei der Pfadi" NZZ 31. Mai 2017
+                        "Wartelisten bei der Pfadi" NZZ
                     </span>
                 </a>
 
@@ -36,11 +35,10 @@
 <div class="tile is-ancestor">
     <div class="tile is-vertical">
         <div class="tile">
-            <div class="tile is-parent is-vertical is-4">
+            <div class="tile is-parent is-vertical is-6">
 
                 <article class="tile is-child box">
-                    <figure class="image is-4by3">
-                        <img src="images/fire.jpg ">
+                        <img src="images/burningDarkFire.jpg ">
                     </figure>
                 </article>                
                 <article class=" tile is-child box ">
@@ -58,7 +56,7 @@
 
                 <article class="tile is-child box">
                 <?php 
-                    require_once('calendar.php');
+                    // require_once('calendar.php');
                 ?>
                 </article>
 
